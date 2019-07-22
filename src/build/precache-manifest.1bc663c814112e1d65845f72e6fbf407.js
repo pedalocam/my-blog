cedd97db273d934eccf72fa79ef33b96b57ac98c
Peter Camerini <Peter.Camerini@skandia.se>
2019-07-22 12:05:26 +0200

    first commit

---
+++ src/build/precache-manifest.1bc663c814112e1d65845f72e6fbf407.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23cc57fe715ceeafc0739c2119b6dd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3669f2f362e55f657d6",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "39d2938b7f6421561502",
+    "url": "/static/js/2.b34c0e3a.chunk.js"
+  },
+  {
+    "revision": "a3669f2f362e55f657d6",
+    "url": "/static/js/main.53e9c6eb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
